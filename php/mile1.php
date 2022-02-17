6fc0ad06791c02b8cf174eca730c97ff2fbbf520
Samuel Scozzari <s_scozzari@libero.it>
2022-02-17 14:48:56 +0100

    Added html structure - Milestone1 completed

--- php/mile1.php
+++ php/mile1.php
@@ -12,16 +12,34 @@ include __DIR__ . '/db/database.php';
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-  <?php
-    foreach ($response as $disc) {
-  ?>
-   <div><?php echo $disc['poster'] ?></div>
-  <?php
-    }
-  ?>
+  <header>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
+      alt="spotify logo"
+      class="logo-spotify"
+    />
+  </header>
+
+  <div class="container">
+    <?php
+      foreach ($response as $disc) {
+    ?>
+      <div class="card-box">
+        <img class="poster-img" src="<?php echo $disc['poster'] ?>" alt="poster" />
+
+        <h2><?php echo $disc['title'] ?></h2>
+
+        <div class="name"><?php echo $disc['author'] ?></div>
+        <div class="date"><?php echo $disc['year'] ?></div>
+      </div>
+    <?php
+      }
+    ?>
+  </div>
 </body>
 
 </html>
