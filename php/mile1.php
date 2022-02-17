cbb0a1fc251ad07ba00dc1fc4be3a68b44951128
Samuel Scozzari <s_scozzari@libero.it>
2022-02-17 15:33:57 +0100

    Title of page reviewed

--- php/mile1.php
+++ php/mile1.php
@@ -11,7 +11,8 @@ include __DIR__ . '/db/database.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Vue-Dischi Backend1</title>
+  
   <link rel="stylesheet" href="../css/style.css">
 </head>
 
